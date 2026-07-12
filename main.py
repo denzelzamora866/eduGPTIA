23544ab85f7bc6e7ac28d25d6ff948915cd6f40f
Denzel Zamora <denzelzamora866@gmail.com>
2026-07-12 00:14:36 -0600

    feat: implement main menu, folder structure and corpus builder

---
+++ main.py
@@ -0,0 +1,21 @@
+from src.models.model import eduGPTIA
+
+def main():
+    model = eduGPTIA()
+
+    while True:
+        option = model.show_menu()
+        if option == "1":
+            model.build_corpus()
+        elif option == "2":
+            model.train()
+        elif option == "3":
+            model.show_information()
+        elif option == "0":
+            print("Saliendo del programa...")
+            break
+        else:
+            print("Opción no válida. Por favor, seleccione una opción válida.")
+
+if __name__ == "__main__":
+    main()
